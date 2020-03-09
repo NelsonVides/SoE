16237062f1c641e79ee84a0cdeda87d42646a9c1
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-03-09 11:12:01 +0100

    Testing basic_protocol
    Here's a template to test the echo protocol we just implemented last
    time. This is how erlang's `common test` framework works. You define a
    suite, with a generic `init_per_suite`, which defines groups of similar
    tests with more specific group-related `init_per_group`, which can
    contain testcase with even more specific `init_per_testcase`. And groups
    can be freely nested, having testcases either on the top-level suite or
    on within any number of nested sub-groups of groups.
    
    I've left you here for now with just one testcase:
    `echo_protocol_does_echo`, where we'll need to implement such test, that
    verifies that we receive back whatever we send through. Notice that for
    now the test explicitly fails (`?assert(false)`), and also notice that
    I've already extracted for you the socket you can use to send data to ;)

---
+++ test/test_ct_SUITE.erl
@@ -0,0 +1,85 @@
+%%%-------------------------------------------------------------------
+%%% @author nelsonvides
+%%% @copyright (C) 2020, nelsonvides
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2020-02-24 11:58:57.704068
+%%%-------------------------------------------------------------------
+-module(test_ct_SUITE).
+
+
+%% API
+-export([all/0,
+         groups/0,
+         init_per_suite/1,
+         end_per_suite/1,
+         init_per_group/2,
+         end_per_group/2,
+         init_per_testcase/2,
+         end_per_testcase/2]).
+
+%% test cases
+-export([
+         echo_protocol_does_echo/1
+        ]).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+all() ->
+    [
+     echo_protocol_does_echo
+    ].
+
+groups() ->
+    [
+    ].
+
+%%%===================================================================
+%%% Overall setup/teardown
+%%%===================================================================
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+
+%%%===================================================================
+%%% Group specific setup/teardown
+%%%===================================================================
+init_per_group(_Groupname, Config) ->
+    Config.
+
+end_per_group(_Groupname, _Config) ->
+    ok.
+
+
+%%%===================================================================
+%%% Testcase specific setup/teardown
+%%%===================================================================
+init_per_testcase(echo_protocol_does_echo, Config) ->
+    ok = application:ensure_started(ranch),
+    {ok, _Listener} = ranch:start_listener(echo_listener,
+                                           ranch_tcp, #{socket_opts => [{port, 5555}]},
+                                           basic_protocol, []),
+    {ok, Sock} = gen_tcp:connect("localhost", 5555, [binary, {packet, 0}]),
+    [{socket, Sock} | Config];
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(echo_protocol_does_echo, Config) ->
+    ok = gen_tcp:close(?config(socket, Config)),
+    ranch:stop_listener(echo_listener),
+    Config;
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%%===================================================================
+%%% Individual Test Cases (from groups() definition)
+%%%===================================================================
+
+echo_protocol_does_echo(Config) ->
+    _Sock = ?config(socket, Config),
+    ?assert(false).
