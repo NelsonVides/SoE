110778d0a612e3acf24800ce86872138a894a087
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-04-03 14:45:51 +0200

    Prototype `parser:decode/1`
    Decoding a message would be a similar business than encoding. Thanks to
    choosing JSON as a text protocol, parsing a binary into an Erlang-like
    JSON structure comes for free using JSON, so getting a working map is
    done for us.
    
    What we need to implement is our particular business logic. What fields
    are mandatory for us? How should each type of stanza look like? It is
    all that logic that we need to implement. Here there are two
    placeholders: "message" for simply messaging somebody, and "status", for
    giving information about errors, successful logins, server informing
    about other clients, etc.
    
    For these two, we have two placeholder for checks, `parse_message` and
    `parse status`, that need to be implemented.

--- src/parser.erl
+++ src/parser.erl
@@ -14,6 +14,19 @@ encode(Msg) ->
     catch _ -> invalid_stanza end.
 
 -spec decode(binary()) -> stanza() | invalid_message.
-decode(_Msg) ->
-    #{}.
+decode(Msg) ->
+    JSON = try jiffy:decode(Msg, [return_maps])
+           catch _ -> invalid_message end,
+    case JSON of
+        #{<<"type">> := <<"message">>} -> parse_message(JSON);
+        #{<<"type">> := <<"status">>} -> parse_status(JSON);
+        _ -> invalid_binary
+    end.
 
+-spec parse_message(map()) -> stanza().
+parse_message(JSON) ->
+    JSON.
+
+-spec parse_status(map()) -> stanza().
+parse_status(JSON) ->
+    JSON.
