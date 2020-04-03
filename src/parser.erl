38439f32a0b6b3730679759e312fed5f80fdbd43
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-04-03 14:40:10 +0200

    Implement `parser:encode/1`
    Voilà! If our message structure is nothing more than a map,
    `jiffy:encode/2` already turns that into a valid JSON in binary that we
    can use to send. Very simple!
    
    If what we're passing was not to have a proper Erlang-like JSON
    structure, jiffy's API says it would throw a `{invalid_ejson, _}`
    as an exception, so we can catch it and return the domain-specific atom
    we want.

--- src/parser.erl
+++ src/parser.erl
@@ -9,8 +9,9 @@
 -type stanza() :: #{}.
 
 -spec encode(stanza()) -> binary() | invalid_stanza.
-encode(_Msg) ->
-    <<"">>.
+encode(Msg) ->
+    try jiffy:encode(Msg)
+    catch _ -> invalid_stanza end.
 
 -spec decode(binary()) -> stanza() | invalid_message.
 decode(_Msg) ->
